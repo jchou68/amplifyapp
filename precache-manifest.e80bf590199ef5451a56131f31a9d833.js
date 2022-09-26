2bbae8cab03e137f20f98689c3b1874d49bdbdb8
johnson-chou8 <johnson.chou6888@gmail.com>
2022-09-26 12:49:20 -0600

    Updates

---
+++ precache-manifest.e80bf590199ef5451a56131f31a9d833.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f874796db21d37420455",
+    "url": "/amplifyapp/static/css/main.02976f2e.chunk.css"
+  },
+  {
+    "revision": "f874796db21d37420455",
+    "url": "/amplifyapp/static/js/main.f874796d.chunk.js"
+  },
+  {
+    "revision": "4e2164a6a842151a7fc5",
+    "url": "/amplifyapp/static/js/1.4e2164a6.chunk.js"
+  },
+  {
+    "revision": "42acf199551df484bef3",
+    "url": "/amplifyapp/static/css/2.85e2934b.chunk.css"
+  },
+  {
+    "revision": "42acf199551df484bef3",
+    "url": "/amplifyapp/static/js/2.42acf199.chunk.js"
+  },
+  {
+    "revision": "92e76797865de90f4d52",
+    "url": "/amplifyapp/static/js/runtime~main.92e76797.js"
+  },
+  {
+    "revision": "f73a8a47b730e54c6bf4834d4650fba5",
+    "url": "/amplifyapp/index.html"
+  }
+];
